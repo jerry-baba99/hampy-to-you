42797c3e70364545dbe97ab9368769731f64347b
jerry-baba99 <relojoariachagas@gmail.com>
2026-07-14 15:38:55 +0530

    Update PhotoGallery.tsx

--- src/components/PhotoGallery.tsx
+++ src/components/PhotoGallery.tsx
@@ -186,7 +186,7 @@ export default function PhotoGallery({ onNext }: PhotoGalleryProps) {
                           {card.caption}
                         </h3>
                         <p className="font-cute text-[10px] md:text-xs text-pink-700/60 mt-1 flex items-center justify-center gap-1">
-                          <RotateCw className="w-3.5 h-3.5" /> Click to read secret letter
+                          <RotateCw className="w-3.5 h-3.5" /> 
                         </p>
                       </div>
                     </div>
